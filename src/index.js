7a65c20a2526803e032081e6cdc5306210f60b2e
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-03-09 17:02:33 +0200

    ._.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const usersServise = new UserService('https://jsonplaceholder.typicode.com/users');
-
-async function usersApp() {
-    const users = await usersServise.requestAllUsers();
-    usersServise.renderAllUsers(users);
-}
-
-usersApp(); 
